8eff6cd940c02c4e752c1022088ba6224f551bd0
Scott Borsch <scottborsch@gmail.com>
2015-09-23 18:49:22 -0500

    160 Lab 4

--- CMWEB160/Lab04/gameIntro.php
+++ CMWEB160/Lab04/gameIntro.php
@@ -1,5 +1,5 @@
-<!--Author:
-	Date:
+<!--	Author: Scott Borsch
+		Date: 9/23/15
 	File:	gameIntro.php
 	Purpose:Chapter 5 Exercise
 -->
@@ -19,15 +19,19 @@
 			$healthTokens = $_POST['healthTokens'];
 			$expTokens = $_POST['expTokens'];
 			$supplyTokens = $_POST['supplyTokens'];
-
+            $password = mb_strtolower($_POST['passcode']);
+            $hardPassword = 'php123';
 				$goldSpent = $healthTokens / 10 + $expTokens / 2 + $supplyTokens / 25;
 			
+            if ( $password == $hardPassword) {
 				print("<h1>You have created $charName the $charType!</h1>");
 				print("<p>$charName has $healthTokens health tokens,
 							$expTokens experience tokens, and 
 							$supplyTokens supply tokens.</p>");
 				print("<p>$charName has spent $goldSpent gold pieces.</p>");
-
+            } else {
+                print("<p>Sorry! That password is NOT correct. Please try again</p>");
+            }
 
 	?>
 
